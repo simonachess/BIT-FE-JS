ee8b07d3a0bf53d0908cb60a15eeeed14eb69867
Simona Limontaite <simonachess@gmail.com>
2021-08-31 12:09:10 +0300

    swiper card

--- _ToDo/index.js
+++ _ToDo/index.js
@@ -254,48 +254,54 @@ class createtedTask {
         })
     }
 
-    static swiper = new Swiper('.swiper', {
-
-        slidesPerView: 1,
-        spaceBetween: 10,
-        // Responsive breakpoints
-        breakpoints: {
-            // when window width is >= 320px
-            320: {
-                slidesPerView: 1,
-                spaceBetween: 20
-            },
-            // when window width is >= 480px
-            480: {
-                slidesPerView: 1,
-                spaceBetween: 30
-            },
-            // when window width is >= 640px
-            640: {
-                slidesPerView: 3,
-                spaceBetween: 40
-            }
-        },
-
-        // Optional parameters
-        direction: 'horizontal',
-        // loop: true,
-        // If we need pagination
-        pagination: {
-            el: '.swiper-pagination',
-        },
-
-        // Navigation arrows
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-
-        // And if we need scrollbar
-        scrollbar: {
-            el: '.swiper-scrollbar',
-        },
+    static swiper = new Swiper(".mySwiper", {
+        effect: "cards",
+        grabCursor: true,
+        loop:true,
     });
+
+    // static swiper = new Swiper('.swiper', {
+
+    //     slidesPerView: 1,
+    //     spaceBetween: 10,
+    //     // Responsive breakpoints
+    //     breakpoints: {
+    //         // when window width is >= 320px
+    //         320: {
+    //             slidesPerView: 1,
+    //             spaceBetween: 20
+    //         },
+    //         // when window width is >= 480px
+    //         480: {
+    //             slidesPerView: 1,
+    //             spaceBetween: 30
+    //         },
+    //         // when window width is >= 640px
+    //         640: {
+    //             slidesPerView: 3,
+    //             spaceBetween: 40
+    //         }
+    //     },
+
+    //     // Optional parameters
+    //     direction: 'horizontal',
+    //     // loop: true,
+    //     // If we need pagination
+    //     pagination: {
+    //         el: '.swiper-pagination',
+    //     },
+
+    //     // Navigation arrows
+    //     navigation: {
+    //         nextEl: '.swiper-button-next',
+    //         prevEl: '.swiper-button-prev',
+    //     },
+
+    //     // And if we need scrollbar
+    //     scrollbar: {
+    //         el: '.swiper-scrollbar',
+    //     },
+    // });
 
 
 }
